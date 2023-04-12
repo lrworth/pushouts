ec8ce090ed04c32e711cfae60e4886b1dad785fa
Luke Worth <luke.worth@bellroy.com>
2023-04-12 12:03:03 +1000

    works

---
+++ MyLib.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE GADTs #-}
+
+module MyLib  where
+
+import Data.Proxy
+
+class Pair tag x a b | tag -> x, tag -> a, tag -> b where
+  projectL :: Proxy tag -> x -> a
+  projectR :: Proxy tag -> x -> b
+
+-- Law: injectL t . projectL t === injectR t . projectR t
+class Pair tag x a b => Pushout tag x a b where
+  type PushoutTo tag x a b
+  injectL :: Proxy tag -> a -> PushoutTo tag x a b
+  injectR :: Proxy tag -> b -> PushoutTo tag x a b
+
+data Wedge a b = Nowhere | Here a | There b
+  deriving Show
+
+data Boring a b
+
+instance Pair (Boring a b) () (Maybe a) (Maybe b) where
+  projectL :: Proxy (Boring a b) -> () -> Maybe a
+  projectL _ () = Nothing
+  projectR :: Proxy (Boring a b) -> () -> Maybe b
+  projectR _ () = Nothing
+
+instance Pushout (Boring a b) () (Maybe a) (Maybe b) where
+  type PushoutTo (Boring a b) () (Maybe a) (Maybe b) = Wedge a b
+  injectL :: Proxy (Boring a b) -> Maybe a -> Wedge a b
+  injectL _ Nothing = Nowhere
+  injectL _ (Just a) = Here a
+  injectR :: Proxy (Boring a b) -> Maybe b -> Wedge a b
+  injectR _ Nothing = Nowhere
+  injectR _ (Just b) = There b
+
+
+ex1 = injectR (Proxy :: Proxy (Boring Int String)) Nothing
+ex2 = injectR (Proxy :: Proxy (Boring Int String)) (Just "hello world")
+ex3 = injectL (Proxy :: Proxy (Boring Int String)) Nothing
+ex4 = injectL (Proxy :: Proxy (Boring Int String)) (Just 13)
